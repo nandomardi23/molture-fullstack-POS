74244ee814cf218ccecca261c6fa96aee42fcfea
nandomardi23 <fernandomardi1@gmail.com>
2023-05-15 14:25:00 +0700

    firts commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+// import "./global.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./sidebar";
+// import { Navbar } from "flowbite-react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Sidebar />
+      <main></main>
+    </div>
+  );
+}
